be60a99e3bf8fcc14d4e927e18b0f7c2fd3cdea1
Daniel Assani <devasddaniel@gmail.com>
2022-09-01 03:14:30 +0200

    exercices android sur l'appareil photo et la camera

---
+++ app/src/main/java/com/daniel/photos/MainActivity.java
@@ -0,0 +1,42 @@
+package com.daniel.photos;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ImageViewCompat;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView conteneur_image;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.action_camera();
+    }
+
+    public void action_camera(){
+        Button start = (Button)findViewById(R.id.button);
+        this.conteneur_image = (ImageView)findViewById(R.id.imageView);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,
+                        0);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        this.conteneur_image.setImageBitmap(bitmap);
+    }
+}
